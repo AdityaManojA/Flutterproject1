420d5b234c5043ec5325571dfa97c35d7a16c24a
AdityaManojA <133322910+Alfin-Jose@users.noreply.github.com>
2024-08-23 22:25:27 +0530

    end of color

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -15,9 +15,11 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.amber.shade400,
             body: Center(
               child: Container(
-                color: Colors.amber.shade500,
+                color: Colors.amber.shade600,
                 height: 300,
                 width: 300,
+                padding: EdgeInsets.only(left: 40,top:50),
+                child: Text("Hello App"),
               ),
             )));
   }
